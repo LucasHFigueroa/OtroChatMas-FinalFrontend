2515edd02d1c7a39779d645df1c43a3fae02a8f4
Lucas Figueroa <lucashernanfigueroa@gmail.com>
2025-09-22 23:55:08 -0300

    correccion en vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/OtroChatMas-FinalFrontend',
+  base: '/OtroChatMas-FinalFrontend/',
   plugins: [react()],
 })
